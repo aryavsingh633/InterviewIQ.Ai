299d62cd99973e362679f7fd9d3add08ef789990
aryavsingh633 <aryavsingh633@gmail.com>
2026-07-06 18:46:44 +0530

    Update CORS origin for production deployment

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ import interviewRouter from './routes/interview.route.js';
 
 const app = express();
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://interviewiq-ai-client-ptxe.onrender.com",
     credentials: true
 }))
 
